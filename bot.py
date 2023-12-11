315d8d447dd3b3f2dd68b16db23b10a705aa2cc4
Alexander Kram <alexander.kram@gmx.de>
2023-12-11 23:33:58 +0100

    jokes has been added

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from asyncio import sleep
 from audioop import reverse
 import time
 import discord
+import random
 
 with open('token.txt') as file:
     token = file.readlines()
@@ -20,23 +21,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('Hallo'):
-        await message.channel.send('Hey, wie geht es dir?')
-    elif message.content.startswith('Gut'):
-        await message.channel.send('Freut mich!')
+
+
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('Countdown'):
+    if (message.content.startswith('Hallo') or message.content.startswith('hallo')):
+        userid = message.author.id
+        string = '<@' + str(userid) + '>'
+        await message.channel.send('Hey, ' + string)
+    elif(message.content.startswith('greeting')):
         await message.channel.send('Counting down from 5')
         arr = range(4)
         res = arr[::-1]
         for x in res:
             time.sleep(1)
             await message.channel.send(x+1)        
-        await message.channel.send('Wie geht es dir <@302056743066796033>?')
+        await message.channel.send('Hallo <@495912448050593794>?')
+    elif(message.content.startswith('joke')):
+
+        joke = ['Egal, wie gut du fährst, Züge fahren Güter.','Wollte Spiderman anrufen – aber der hatte kein Netz.','Wenn sich Wissenschaftler ein Brot belegt, ist es dann wissenschaftlich belegt?','Was machen Mathematiker im Garten? – Wurzeln ziehen','Steht ein Baum allein im Wald.','Übrigens: ich habe gerade ein Blatt gelocht… Aber das nur am Rande.','Was versteht man unter einer Turbine? Nichts – ist viel zu laut!']
+        await message.channel.send(random.choice(joke))
+        
+
 client.run(token[0])
 
 
