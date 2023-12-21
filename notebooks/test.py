1b64ba1439d85432e9e954d7c7205683b94ca4e7
Alexander Kram <alexander.kram@gmx.de>
2023-12-21 17:55:29 +0100

    having fun with python-turtle

--- notebooks/test.py
+++ notebooks/test.py
@@ -2,3 +2,13 @@ print(2+2)
 print("Hund" + "Katze")
 print("Hund" + str(1))
 print("1"+"1")
+x = 1
+y = "hallo"
+x1 = 2
+print(x+x1)
+print(type (x))
+print(type(y))
+
+A = {1,2,3}
+B = {1,2}
+print(A-B)
