3c80f3e354c7c2799997ed6597916b71b522d256
Alexander Kram <alexander.kram@gmx.de>
2024-01-12 12:56:47 +0100

    many new features

--- bot3.py
+++ bot3.py
@@ -7,7 +7,9 @@ from discord.ext import commands
 import pytz
 import requests
 from icalendar import Calendar
-from datetime import datetime
+from datetime import date, datetime, timedelta
+
+data = None
 
 # opening token from token.txt
 with open('token.txt') as file:
@@ -17,22 +19,42 @@ with open('token.txt') as file:
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="bot!", intents=intents)
-bot.remove_command("help")
+#bot.remove_command("help")
 
 # Log-In-Confirmation
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+class SimpleView(discord.ui.View):
+      
+      @discord.ui.button(label="Yesterday",style=discord.ButtonStyle.blurple)
+      async def hello1(self,interaction:discord.Interaction,button: discord.ui.Button):
+             date_entry=date.today() - timedelta(days=1)
+             await lectureplan(date_entry)
+      @discord.ui.button(label="Today",style=discord.ButtonStyle.green)
+      async def hello2(self,interaction:discord.Interaction,button: discord.ui.Button):
+             date_entry=date.today()
+             await lectureplan(date_entry)
+      @discord.ui.button(label="Tomorrow",style=discord.ButtonStyle.blurple)
+      async def hello3(self,interaction:discord.Interaction,button: discord.ui.Button):
+             date_entry=date.today() + timedelta(days=1)
+             await lectureplan(date_entry)
+        
 # command for getting the actual lecture plan
 @bot.command()
 # input command has to be in format bot!lecture YEAR-MO-DA
-async def lecture(ctx,date_entry):
-    async with ctx.typing():
-        await asyncio.sleep(2)
-    year, month, day = map(int, date_entry.split('-'))
+async def lecture(ctx):
+    global data
+    data = ctx
+    view =SimpleView()
+    await ctx.send(view=view)
+    
+async def lectureplan(date_entry):
+    global data
+    #year, month, day = map(int, date_entry.split('-'))
     cal_url = "https://stuv.app/MOS-TINF23A/ical"
-    target_date = datetime(year, month, day) #style 2023, 12, 20
+    target_date = date_entry #style 2023, 12, 20
     response = requests.get(cal_url)
     if response.status_code == 200:
                 # Parse the iCal data
@@ -45,13 +67,11 @@ async def lecture(ctx,date_entry):
                 target_date_events = []
                 for event in cal.walk('VEVENT'):
                     start_time = event.get('dtstart').dt
-
-                    if start_time.date() == target_date.date():
+                    if start_time.date() == target_date:
                         summary = event.get('summary')
                         end_time = event.get('dtend').dt
 
                         # Convert UTC+0 to UTC+1
-                      
                         target_timezone = pytz.timezone('Europe/Paris')  # Replace with your target timezone
                         start_time = start_time.astimezone(target_timezone)
                         end_time = end_time.astimezone(target_timezone)
@@ -77,13 +97,13 @@ async def lecture(ctx,date_entry):
                     #print(f"Start Time: {event['start_time']}")
                     print(f"End Time: {event['end_time']}")
                     print("-----")
-                    await ctx.send(embed=embed)
+                    await data.send(embed=embed) 
 
                         
     else:
                 print(f"Failed to retrieve the iCal data. Status code: {response.status_code}")
-
     
 
-# running bot
+
+# running the bot
 bot.run(token[0])
