1dbe4b182fe454936b3bbbf3016e75b1e8d86945
Alexander Kram <alexander.kram@gmx.de>
2024-01-14 02:32:35 +0100

    trying to find a solution..

--- bot3.py
+++ bot3.py
@@ -10,7 +10,7 @@ from icalendar import Calendar
 from datetime import date, datetime, timedelta
 
 data = None
-
+counter = 0
 # opening token from token.txt
 with open('token.txt') as file:
     token = file.readlines()
@@ -69,48 +69,52 @@ async def lectureplan(date_entry):
             
                 # Parse the iCal data using the icalendar library
                 cal = Calendar.from_ical(cal_data)
-
                 # Extract and print events
                 target_date_events = []
+                print("Hello?!")
+                
                 for event in cal.walk('VEVENT'):
-                    start_time = event.get('dtstart').dt
-                    if start_time.date() == target_date:
-                        summary = event.get('summary')
-                        end_time = event.get('dtend').dt
-
-                        # Convert UTC+0 to UTC+1
-                        target_timezone = pytz.timezone('Europe/Paris')  # Replace with your target timezone
-                        start_time = start_time.astimezone(target_timezone)
-                        end_time = end_time.astimezone(target_timezone)
-                        target_date_events.append({
-                            'summary': summary,
-                            'start_time': start_time.strftime("%H:%M" + ' Uhr'),
-                            'end_time': end_time.strftime("%H:%M" + ' Uhr')
-                        })
-                    
+                        start_time = event.get('dtstart').dt
+                        counter = 0
+                        counter = counter + 1
+                        list = [counter]
+                        print(list)
+                        if start_time.date() == target_date:
+                            summary = event.get('summary')
+                            end_time = event.get('dtend').dt
+                        
+                            # Convert UTC+0 to UTC+1
+                            target_timezone = pytz.timezone('Europe/Paris')  # Replace with your target timezone
+                            start_time = start_time.astimezone(target_timezone)
+                            end_time = end_time.astimezone(target_timezone)
+                            target_date_events.append({
+                                'summary': summary,
+                                'start_time': start_time.strftime("%H:%M" + ' Uhr'),
+                                'end_time': end_time.strftime("%H:%M" + ' Uhr')
+                            })
+            
+                       
             # Print all events for the target date
                 for event in target_date_events:
-                    embed = discord.Embed(
-       
-                    title=event['summary'],
-                    #description=date,
-                    color=discord.Color.blue()  # You can set the color of the embed
-                    )
-                    # Add fields to the embed
-                    embed.add_field(name='Beginn', value=event['start_time'], inline=False)
-                    embed.add_field(name='Ende', value=event['end_time'], inline=True)
-                   
-                    print(f"Summary: {event['summary']}")
-                    #print(f"Start Time: {event['start_time']}")
-                    print(f"End Time: {event['end_time']}")
-                    print("-----")
-                    await data.send(embed=embed) 
+                            embed = discord.Embed(
+            
+                            title=event['summary'],
+                            #description=date,
+                            color=discord.Color.blue()  # You can set the color of the embed
+                            )
+                            # Add fields to the embed
+                            embed.add_field(name='Beginn', value=event['start_time'], inline=False)
+                            embed.add_field(name='Ende', value=event['end_time'], inline=True)
+                        
+                            print(f"Summary: {event['summary']}")
+                            #print(f"Start Time: {event['start_time']}")
+                            print(f"End Time: {event['end_time']}")
+                            print("-----")
+                            await data.send(embed=embed) 
 
                          
     else:
                 print(f"Failed to retrieve the iCal data. Status code: {response.status_code}")
     
-
-
 # running the bot
 bot.run(token[0])
