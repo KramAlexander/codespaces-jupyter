99c4dbeed5d8b2d0444d27f4e2aa4845973d138c
Alexander Kram <alexander.kram@gmx.de>
2024-01-27 01:34:54 +0100

    I added roll a dice :)

---
+++ small programs/rolladice.py
@@ -0,0 +1,9 @@
+import random
+
+# creating a list with number (= dice)
+dice = [1,2,3,4,5,6]
+
+# printing random number 
+print("-----------------")
+print(f"The dice says: {random.choice(dice)}")
+print("-----------------")
