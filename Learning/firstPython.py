b1d1c67f969a15b7d64231eed1a1f3fe74f4840d
Alexander Kram <alexander.kram@gmx.de>
2024-02-11 20:12:11 +0100

    :)

--- Learning/firstPython.py
+++
@@ -1,6 +0,0 @@
-# this is my first python programm so pls dont judge :)
-
-cars = ["audi","bmw","porsche","mercedes"]
-for x in cars:
-    print(x)
-#test
--- small programs/rolladice.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# creating a list with number (= dice)
-dice = [1,2,3,4,5,6]
-
-# printing random number 
-print("-----------------")
-print(f"You rolled a {random.choice(dice)}!")
-print("-----------------")
